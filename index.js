38b6b50cb1ec20a018d837d4ac372e651d6d591e
Guillermo Bereche <bere0058@algonquinlive.com>
2022-01-21 21:05:05 -0500

    first

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 /* Variables */
-const btnHeight = document.querySelector('.btn-1');
-const btnHeightAfter = window.getComputedStyle(btnHeight,':after');
-const btnSpan = document.querySelectorAll('.btn-span');
+const btnHeight = document.querySelector('.btn-div');
+
 /* Giving btn-span height */
-btnSpan.forEach(item => {
-    item.style.height = btnHeightAfter.height;
+if (btnHeight) {
+    const btnHeightAfter = window.getComputedStyle(btnHeight,':before');
+    const btnSpan = document.querySelectorAll('.btn-span');
+    btnSpan.forEach(item => {
+        item.style.height = btnHeightAfter.height;
+    })
+}
+
+/* Navbar Interaction */
+const main = document.querySelector('main');
+const navBtn = document.querySelector('.nav-btn');
+const navDisplay = document.querySelector('.nav-display');
+
+navBtn.addEventListener('click', () => {
+    navDisplay.classList.toggle('nav-display-move');
+    main.classList.toggle('blur-effect');
 })
